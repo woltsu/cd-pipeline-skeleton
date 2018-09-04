0fb59c6bf20390119726b72032ff83f32433136f
Olli <oltsuwh@gmail.com>
2018-09-04 13:17:41 +0300

    added additional npm packages

--- src/server.js
+++ src/server.js
@@ -1,13 +1,28 @@
+// IMPORTS
 const http = require('http')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const compression = require('compression')
+const config = require('./config')
 const app = require('express')()
-const { port } = require('../config')
 
-const exampleRouter = require('./controllers/example')
-app.use('/', exampleRouter)
+// MIDDLEWARE
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan('common'))
+app.use(helmet())
+app.use(compression())
 
+// CONTROLLERS
+const exampleController = require('./controllers/example')
+app.use('/', exampleController)
+
+// CREATE SERVER
 const server = http.createServer(app)
-server.listen(port, () => {
-  console.log(`Listening on port ${ port }`)
+server.listen(config.port, () => {
+  console.log(`Listening on port ${ config.port }`)
 })
 
 module.exports = {
--- tests/controllers/example.test.js
+++ tests/controllers/example.test.js
@@ -2,10 +2,14 @@ const supertest = require('supertest')
 const { app, server } = require('../../src/server')
 const api = supertest(app)
 
-test('example test', async () => {
-  await api
-    .get('/')
-    .expect(200)
+describe('example controller', () => {
+  describe('/', () => {
+    test('GET', async () => {
+      const response = await api.get('/')
+      expect(response.status).toBe(200)
+      expect(response.text).toBe('Hello world!')
+    })
+  })
 })
 
 afterAll(() => {
